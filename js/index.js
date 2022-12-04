e6bf29f80e474f868caed74ab2eb0f2df8831256
Maksym67 <maxpasternak54@gmail.com>
2022-12-04 05:19:02 +0200

    update

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
+// Text type animation
+
 var typed = new Typed('.typed', {
     strings: ["Designer", "Developer", "Freelancer", "Photographer"], 
     loop: true,
     typeSpeed: 100,
     backSpeed: 50,
     backDelay: 2000
+});
+
+// Scroll trigger
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if(scroll >= 150) {
+        $("#header").addClass('header-scrolled');
+        $(".back-to-top").addClass('back-to-top-active');
+    } else {
+        $("#header").removeClass('header-scrolled');
+        $(".back-to-top").removeClass('back-to-top-active');
+    }
 });
